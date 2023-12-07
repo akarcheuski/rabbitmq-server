6159bd560c37e1e8b00017b21094854526daffaa
Andrei Karcheuski <andrei@tileg.com>
2023-12-07 19:13:06 +0400

    Added Bundle config

---
+++ RabbitMQ/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Optimization;
+
+namespace RabbitMQ.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/script").Include(
+                        "~/Scripts/script.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                     "~/Content/bootstrap.css",
+                     "~/Content/bootstrap-theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+        }
+    }
+}
