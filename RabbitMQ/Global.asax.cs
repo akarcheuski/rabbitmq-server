e0ba8558e278abd7343d0e1ffff4b364e8007a1b
Andrei Karcheuski <andrei@tileg.com>
2023-12-07 19:11:24 +0400

    Created RabbitMQServer project

---
+++ RabbitMQ/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.App_Start;
+using System;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace RabbitMQ
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            IoCConfig.RegisterDependencies();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            System.Web.HttpContext.Current.Session["MessagesQuantity"] = 0;
+            System.Web.HttpContext.Current.Session["Time"] = 0;
+        }
+    }
+}
