aa1fdedf5c694a58d30f155662b39e68ce460dce
Andrei Karcheuski <andrei@tileg.com>
2023-12-07 19:22:26 +0400

    Created Models

---
+++ RabbitMQ/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace RabbitMQ.Models
+{
+    public class Client
+    {       
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ RabbitMQ/Models/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RabbitMQ.Models
+{
+    public class MainViewModel
+    {
+        [Required]
+        public string ClientId { get; set; }
+
+        public IEnumerable<Client> Clients { get; set; }
+    }
+}
