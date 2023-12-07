729ed029f4d1603647d5c5811c55d0d8991645d7
Andrei Karcheuski <andrei@tileg.com>
2023-12-07 19:13:55 +0400

    Added Route config

---
+++ RabbitMQ/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RabbitMQ
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "SendMessages",
+               url: "Default/SendMessages",
+               defaults: new { controller = "Default", action = "SendMessages" }
+            );
+            routes.MapRoute(
+             name: "StopSending",
+             url: "Default/StopSending",
+             defaults: new { controller = "Default", action = "StopSending" }
+            );
+        }     
+    }
+}
